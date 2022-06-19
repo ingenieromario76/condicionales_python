31c752b2579f25359c14aaa06feaf3bee9567012
unknown <ingenieromario@hotmail.com>
2022-06-19 09:49:25 -0300

    avances

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -31,10 +31,69 @@ no se busca utilizar bucles o algoritmos de ordenamiento ya que aún no hemos ll
 contenido.
 '''
 
+from operator import truediv
+
+
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
-texto_1 = input("Ingrese la 1° Palabra:")
-texto_2 = input("Ingrese la 2° Palabra:")
-texto_3 = input("Ingrese la 3° Palabra:")
-respuesta = input("Que desea hacer? 1- Ordenar por orden alfabético o 2- Ordenar por cantidad de letras")
+palabra_1 = input("Ingrese la 1° Palabra:")
+palabra_2 = input("Ingrese la 2° Palabra:")
+palabra_3 = input("Ingrese la 3° Palabra:")
+
+opcion = input("Que desea hacer? 1- Ordenar por orden alfabético o 2- Ordenar por cantidad de letras:")
+
+if opcion=='1':
+    if palabra_1>palabra_2 and palabra_1>palabra_3:
+        if palabra_2>palabra_3:
+            print("Orden: ",palabra_1,palabra_2,palabra_3)
+        else:
+            print("Orden: ",palabra_1,palabra_3,palabra_2)
+    elif palabra_2>palabra_1 and palabra_2>palabra_3:   
+        if palabra_1>palabra_3:
+            print("Orden: ",palabra_2,palabra_1,palabra_3)
+        else:
+            print("Orden: ",palabra_2,palabra_3,palabra_1)
+    elif palabra_3>palabra_1 and palabra_3>palabra_2:   
+        if palabra_1>palabra_2:
+            print("Orden: ",palabra_3,palabra_1,palabra_2)
+        else:
+            print("Orden: ",palabra_3,palabra_2,palabra_1)
+
+if opcion=='2':
+    if len(palabra_1)>len(palabra_2) and len(palabra_1)>len(palabra_3):
+        if len(palabra_2)>len(palabra_3):
+            print("Orden: ",palabra_1,palabra_2,palabra_3)
+        else:
+            print("Orden: ",palabra_1,palabra_3,palabra_2)
+    elif len(palabra_2)>len(palabra_1) and len(palabra_2)>len(palabra_3):   
+        if len(palabra_1)>len(palabra_3):
+            print("Orden: ",palabra_2,palabra_1,palabra_3)
+        else:
+            print("Orden: ",palabra_2,palabra_3,palabra_1)
+    elif len(palabra_3)>len(palabra_1) and len(palabra_3)>len(palabra_2):   
+        if len(palabra_1)>len(palabra_2):
+            print("Orden: ",palabra_3,palabra_1,palabra_2)
+        else:
+            print("Orden: ",palabra_3,palabra_2,palabra_1)
+
+
+print ("Con Listas y bucles!!")
+print ("=====================")
+palabras = []
+palabras.append(palabra_1)
+palabras.append(palabra_2)
+palabras.append(palabra_3)
+
+opcion = input("Que desea hacer? 1- Ordenar por orden alfabético o 2- Ordenar por cantidad de letras:")
+lista_ordenada = []
+
+if opcion=='1':
+    lista_ordenada = sorted(palabras)
+    # Ordena inverso: lista_ordenada = sorted(palabras,reverse = True)
+    print(lista_ordenada)
+elif opcion=='2':
+    lista_ordenada = sorted(palabras, key = len)
+    print(lista_ordenada)
+
+
 
